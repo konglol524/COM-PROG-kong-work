2559ca60896b6fa2364f9776203de493debee4d4
Kongbhumi <97303018+konglol524@users.noreply.github.com>
2023-08-15 22:32:18 +0700

    Create bisection_loop12.cpp
    15/8/2023

---
+++ bisection_loop12.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+
+int main() {
+     double L = 0;
+     double a; std::cin >> a;
+     double U = sqrt(a);
+     double x = (L + U) / 2.0;
+
+     double aprox = pow(10, x);
+
+    while(fabs(aprox - a) > (1e-10 * std::max(a, aprox))){
+        
+        if (pow(10, x) > a){
+            U = x;
+        } else if (pow(10, x) < a) {
+            L = x;
+        }
+
+        x = (L + U) / 2.0;
+        aprox = pow(10, x);
+        
+    }
+
+    std::cout << round(x * 1000000) / 1000000.0 << std::endl;
+
+}
